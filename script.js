ab7e208b4692a2a898d4aad3100385cfb312a0a5
Stephen B <sbelyea@ecentricarts.com>
2016-03-07 17:16:09 -0500

    Add settings for image widths and sketch steps for setting columns.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 jQuery(document).ready(function($) {
   $('#toggle-settings').on('click', handleSettingsToggle);
+  $('#update-settings').on('submit', handleSettingsUpdate);
 });
+
+var sizes = {
+  mcw : 320,
+  miw : 320,
+  dcw : 1200,
+  diw : 1700
+};
+
 var handleSettingsToggle = function (e) {
   var togStat = this.getAttribute('data-settings-toggle'),
       setPnl = document.getElementById('settings');
@@ -13,5 +22,27 @@ var handleSettingsToggle = function (e) {
     this.setAttribute('data-settings-toggle', false);
     this.innerHTML = "Open Settings";
     setPnl.setAttribute('data-settings-open', false);
+  }
+};
+
+var handleSettingsUpdate = function (e) {
+  e.preventDefault();
+  var mcwInp = document.getElementById('mobile-content-wide'),
+      dcwInp = document.getElementById('desktop-content-wide'),
+      mcwVal = mcwInp.value,
+      dcwVal = dcwInp.value,
+      updates = 0;
+  if ( mcwVal !== sizes.mcw ) {
+    sizes.mcw = mcwVal;
+    updates++;
+  }
+  if ( dcwVal !== sizes.dcw ) {
+    sizes.dcw = dcwVal;
+    updates++;
+  }
+  if ( updates > 0 ) {
+    // -- Set column widths for mobile and desktop.
+    // -- If diff from content to image widths, set image
+    //    sizes and negative margin within view wrapper.
   }
 };
